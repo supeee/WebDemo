1ac74a58c3982371dc4f17c14ae973eb98eb7cfe
Petri Saarelainen <p.saarelainen@gmail.com>
2019-02-24 14:01:58 +0200

    Lisätty autot

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
@@ -43,6 +43,34 @@ namespace AspNetCoreBackend.Controllers
             return View();
         }
 
+        public IActionResult Autot()
+        {
+            List<Auto> autot = new List<Auto>()
+            {
+                new Auto()
+                {
+                    Merkki = "Ford",
+                    Malli = "Focus",
+                    Huippunopeus = 190
+                },
+                new Auto()
+                {
+                    Merkki = "Toyota",
+                    Malli = "Avensis",
+                    Huippunopeus = 220
+                },
+                new Auto()
+                {
+                    Merkki = "Porsche",
+                    Malli = "911",
+                    Huippunopeus = 300
+                }
+            };
+
+            return View(autot);
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ AspNetCoreBackend/AspNetCoreBackend/Models/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBackend.Models
+{
+    public class Auto
+    {
+        public string Merkki { get; set; }
+
+        public string Malli { get; set; }
+
+        public int Huippunopeus { get; set; }
+    }
+}
